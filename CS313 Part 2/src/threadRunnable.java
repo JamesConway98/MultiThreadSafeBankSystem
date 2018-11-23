7aface637bf24c8d971379257234dd252e8b3223
Scott MacKenzie SE2016 <scott.mackenzie.2016@uni.strath.ac.uk>
2018-11-23 11:59:34 +0000

    Add new file

---
+++ CS313 Part 2/src/threadRunnable.java
@@ -0,0 +1,15 @@
+public class threadRunnable implements Runnable {
+
+    private int one;
+
+    public threadRunnable() {
+        one = 1;
+    }
+
+    public void run() {
+
+        while (one == 1) {
+        }
+    }
+
+}
