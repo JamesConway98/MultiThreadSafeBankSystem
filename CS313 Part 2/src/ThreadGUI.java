c59b64f6e589d0e883a4236fefddb05a764e0e4f
Scott MacKenzie SE2016 <scott.mackenzie.2016@uni.strath.ac.uk>
2018-11-23 11:58:05 +0000

    Update ThreadGUI.java

--- CS313 Part 2/src/ThreadGUI.java
+++ CS313 Part 2/src/ThreadGUI.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class ThreadGUI implements ActionListener {
 
@@ -13,14 +14,14 @@ public class ThreadGUI implements ActionListener {
 
     public ThreadGUI() {
         threadMonitor = new ThreadMonitor();
-        buildGUI();
+        buildGUI(threadMonitor.getThreads());
     }
 
-    public void buildGUI() {
+    public void buildGUI(Thread[] threads) {
         threadFrame = new JFrame("Thread Monitor");
         threadPanel = new JPanel(new GridLayout(3, 2));
 
-        threadTable = new ThreadTable(threadMonitor.getThreads());
+        threadTable = new ThreadTable(threads);
 
         refreshButton = new JButton("Refresh");
         refreshButton.addActionListener(this);
@@ -50,54 +51,93 @@ public class ThreadGUI implements ActionListener {
         threadFrame.setVisible(true);
     }
 
+    private void refreshFrame(Thread[] threads) {
+        threadTable.clearTable();
+        ArrayList<Thread> noNulls = threadTable.clearNulls(threads);
+        for (Thread thread : noNulls) {
+            threadTable.updateTable(thread);
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 
         switch (actionEvent.getActionCommand()) {
             case "Refresh": {
-                System.out.println("\n");
                 threadMonitor.monitorThreads();
-                threadTable.buildTable(threadMonitor.getThreads());
-                threadFrame.getContentPane().removeAll();
-                threadFrame.dispose();
-                this.buildGUI();
+                refreshFrame(threadMonitor.getThreads());
                 break;
             }
             case "Search by Name": {
                 String name = JOptionPane.showInputDialog(threadPanel, "Please enter the name of the thread you want.", null);
+                if (name == null) {
+                    break;
+                }
                 Thread thread = threadMonitor.searchThreads(name);
                 if (thread != null) {
-                    threadMonitor.printThread(thread);
+                    threadTable.clearTable();
+                    threadTable.updateTable(thread);
+                    break;
                 } else {
-                    System.out.println("Could not find thread");
+                    JOptionPane.showMessageDialog(null, "Please enter a valid name");
                 }
                 break;
             }
             case "Filter by Thread Group": {
                 String name = JOptionPane.showInputDialog(threadPanel, "Please enter the name of the thread group you want.", null);
+                if (name == null) {
+                    break;
+                }
                 ThreadGroup threadGroup = threadMonitor.filterByThreadGroup(name);
                 if (threadGroup != null) {
                     Thread[] threads = new Thread[threadGroup.activeCount()];
                     while (threadGroup.enumerate(threads, true) == threads.length) {
                         threads = new Thread[threads.length * 2];
                     }
-                    for (Thread thread : threads) {
-                        threadMonitor.printThread(thread);
-                    }
+                 refreshFrame(threads);
                 } else {
-                    System.out.println("Could not find thread");
+                    JOptionPane.showMessageDialog(null, "Please enter a valid name");
                 }
                 break;
             }
             case "Start Thread": {
+                String name = JOptionPane.showInputDialog(threadPanel, "Please enter the name of the thread you want to add.", null);
+                if (name == null) {
+                    break;
+                }
+                if (!name.equals("")) {
+                    ThreadGroup threadGroup = Thread.currentThread().getThreadGroup();
+                    threadRunnable tr = new threadRunnable();
+                    Thread thread = new Thread(threadGroup, tr,  name);
+                    thread.start();
+                    JOptionPane.showMessageDialog(null, "Thread validated. Adding...");
+                    threadTable.updateTable(thread);
+                    threadMonitor.monitorThreads();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Please enter a valid name");
+                }
                 break;
             }
             case "Stop Thread": {
+                String name = JOptionPane.showInputDialog(threadPanel, "Please enter the name of the thread you want to terminate.", null);
+                if (name == null) {
+                    break;
+                }
+                Thread thread = threadMonitor.searchThreads(name);
+                if (thread != null) {
+                    thread.stop();
+                    JOptionPane.showMessageDialog(null, "Thread validated. Stopping...");
+                    threadTable.deleteRowAtThread(thread);
+                    refreshFrame(threadMonitor.getThreads());
+                } else {
+                    JOptionPane.showMessageDialog(null, "Please enter a valid name");
+                }
                 break;
             }
             case "Exit": {
                 System.exit(0);
                 break;
+
 
             }
         }
