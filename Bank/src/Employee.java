be211b3abf8db67a7bc654121c1c7eca5673a2d3
James Conway CS2016 <james.conway.2016@uni.strath.ac.uk>
2018-11-23 16:17:37 +0000

    Replace Employee.java

--- Bank/src/Employee.java
+++ Bank/src/Employee.java
@@ -1,17 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Employee {
+public class Employee extends User{
 
     List<Customer> customerList = new ArrayList<>();
+    int id;
+
+    public Employee(int id, List<Customer> customerList) {
+        this.id = id;
+        this.customerList = customerList;
+    }
 
     public void addCustomer(Customer customer){
         customerList.add(customer);
     }
 
-    public void transfer(double amount, Customer customer1, Customer customer2, int transfereeNum, int accountNo) {
-        Account transfererAccount = customer1.getAccountByNo(accountNo);
-        transfererAccount.transfer(amount, customer2, transfereeNum);
+    public int getId(){
+        return id;
     }
+
+//    public void transfer(double amount, Customer customer1, Customer customer2, int transfereeNum, int accountNo) {
+//        Account transfererAccount = customer1.getAccountByNo(accountNo);
+//        transfererAccount.transfer(amount, customer2, transfereeNum);
+//    }
 
 }
