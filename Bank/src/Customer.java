c6c87f6b214a5a114e63112c749f8a5eb9ea91b0
James Conway CS2016 <james.conway.2016@uni.strath.ac.uk>
2018-11-23 16:17:02 +0000

    Replace Customer.java

--- Bank/src/Customer.java
+++ Bank/src/Customer.java
@@ -3,73 +3,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class Customer {
+public class Customer extends User{
 
+    List<Integer> myAccountList = new ArrayList<>();
     String username;
-    String password;
-    private int id;
 
-    List<Account> myAccountList = new ArrayList<>();
-
-    public Customer(String username, String password) {
+    public Customer(String username) {
         this.username = username;
-        this.password = password;
+    }
+
+    public void addAccount(int accountNumber) {
+        myAccountList.add(accountNumber);
     }
 
     public String getUsername() {
         return username;
     }
 
-    public Account getAccountByNo(int accountNo) {
-        for (Account account : myAccountList) {
-            if (account.getAccountNum() == accountNo) {
-                return account;
-            }
-        }
-        return null;
-    }
 
-    public boolean addAccount(char accountType, int accountNumber) {
-        Account newAccount;
-        switch (accountType) {
-            case ('k'):
-                newAccount = new KidsAccount(accountNumber);
-                break;
-            case ('s'):
-                newAccount = new SavingsAccount(accountNumber);
-                break;
-            case ('c'):
-                newAccount = new CurrentAccount(accountNumber);
-                break;
-            default:
-                System.out.println("Error");
-                return false;
-        }
-        myAccountList.add(newAccount);
-        return true;
-    }
-
-    public Double getBalance(int accountNo) {
-        Account account = getAccountByNo(accountNo);
-        if (account != null) {
-            return getAccountByNo(accountNo).getBalance();
-        } else {
-            return null;
-        }
-    }
-
-    public void deposit(double amount, int accountNo) {
-        getAccountByNo(accountNo).deposit(amount);
-    }
-
-    public void withdraw(double amount, int accountNo) {
-       getAccountByNo(accountNo).withdraw(amount);
-    }
-
-    public void transfer(double amount, Customer customer, int transfereeNum, int accountNo) {
+    /*public void transfer(double amount, Customer customer, int transfereeNum, int accountNo) {
         this.getAccountByNo(accountNo).transfer(amount, customer, transfereeNum);
-    }
+    }*/
 
+    public boolean checkMyAccounts(int account) {
+        for (int acnt: myAccountList){
+            if (acnt == account)
+                return true;
+        }
+        return false;
+    }
 
 }
 
