48fe62bfc29278a774179e6dc2e679466c60493e
James Conway CS2016 <james.conway.2016@uni.strath.ac.uk>
2018-11-23 16:18:28 +0000

    Replace TransferRunnable.java

--- Bank/src/TransferRunnable.java
+++ Bank/src/TransferRunnable.java
@@ -2,45 +2,26 @@
 public class TransferRunnable implements Runnable {
 
     private static final int DELAY = 1;
-
-    private Employee employee;
-    private Customer customer1, customer2;
     private double amount;
     private int c1AccountNum, c2AccountNum;
+    private Manager manager;
 
-    public TransferRunnable(Customer c1, Customer c2, double a, int c1an, int c2an) {
-        customer1 = c1;
-        customer2 = c2;
+
+    public TransferRunnable(double a, int c1an, int c2an, Manager manager) {
         amount = a;
         c1AccountNum = c1an;
         c2AccountNum = c2an;
+        this.manager = manager;
     }
 
-    public TransferRunnable(Employee employee, Customer c1, Customer c2, double a, int c1an, int c2an) {
-        this.employee = employee;
-        customer1 = c1;
-        customer2 = c2;
-        amount = a;
-        c1AccountNum = c1an;
-        c2AccountNum = c2an;
-    }
+   public void run() {
 
-    public void run() {
-        if(employee == null) {
-            try {
-                customer1.transfer(amount, customer2, c2AccountNum, c1AccountNum);
-                Thread.sleep(DELAY);
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
-        }
-        else{
-            try {
-                employee.transfer(amount, customer1, customer2, c2AccountNum, c1AccountNum);
-                Thread.sleep(DELAY);
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
+        try {
+            manager.getAccountByNumber(c1AccountNum).withdraw(amount);
+            manager.getAccountByNumber(c2AccountNum).deposit(amount);
+            Thread.sleep(DELAY);
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
         }
 
     }
